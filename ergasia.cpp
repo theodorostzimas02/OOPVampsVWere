d375d0df9b75ed3aba019ea7798178fc2e49048e
Theodoros Tzimas <79987161+sdi2000190@users.noreply.github.com>
2022-12-19 16:14:02 +0200

    Add files via upload

--- ergasia.cpp
+++ ergasia.cpp
@@ -31,7 +31,7 @@ class Entity{
             return y;
         }
 
-        void SetXY(int x,int y){
+        void SetYX(int y,int x){
             this->x=x;
             this->y=y;
         }
@@ -56,7 +56,8 @@ class NPC : public Entity {
             Defence_Lvl = (rand() % 2) + 1;
         }
 
-        virtual void Move(int &x, int &y, short int MoveN){
+        // Η Move() του καθε NPC χειρίζεται την κίνησή του μεσα στον χάρτη και παίρνει ως όρισμα το index ενος array από την Scan() παρακάτω
+        virtual void Move(short int MoveN){
             return;
         }
 
@@ -88,7 +89,8 @@ class NPC : public Entity {
 
         void Attack(NPC* target)
         {
-            target->CurrentHP=target->CurrentHP-(target->Defence_Lvl - Power_Lvl);
+            target->CurrentHP=target->CurrentHP-(this->Power_Lvl - target->Defence_Lvl);
+            if (target->Defence_Lvl > 0) target->Defence_Lvl--;
         }
 
 };
@@ -182,19 +184,22 @@ class Vampire : public NPC {
     public:
         Vampire() { ID = 'v'; }
 
-        void Move(int &x, int &y, short int MoveN){
+        void Move(short int MoveN){
             switch (MoveN)
             {
-                case 0:     // Μένει ακίνητο
-	                break;
-
+                case 0:    // Κινείται διαγώνια αριστερά και πάνω
+                    this->y--;
+                    this->x--;
+                    break;
+                
                 case 1:     // Κινείται πάνω
                     this->y--;
                     break;
-
-                case 2:    // Κινείται κάτω
-                    this->y++;
-                    break;
+                    
+                case 2:    // Κινείται διαγώνια δεξιά και πάνω
+                    this->y--;
+                    this->x++;
+                    break;    
 
                 case 3:    // Κινείται αριστερά
                     this->x--;
@@ -204,22 +209,16 @@ class Vampire : public NPC {
                     this->x++;
                     break;
 
-                case 5:    // Κινείται διαγώνια αριστερά και πάνω
-                    this->y--;
-                    this->x--;
-                    break;
-
-                case 6:    // Κινείται διαγώνια δεξιά και πάνω
-                    this->y--;
-                    this->x++;
-                    break;
-
-                case 7:    // Κινείται διαγώνια αριστερά και κάτω
+                case 5:    // Κινείται διαγώνια αριστερά και κάτω
                     this->y++;
                     this->x--;
                     break;
 
-                case 8:    // Κινείται διαγώνια δεξιά και κάτω
+                case 6:    // Κινείται κάτω
+                    this->y++;
+                    break;    
+
+                case 7:    // Κινείται διαγώνια δεξιά και κάτω
                     this->y++;
                     this->x++;
                     break;
@@ -233,23 +232,21 @@ class Werewolf : public NPC {
     public:
         Werewolf() { ID = 'w';}
 
-        void Move(int &x, int &y, short int MoveN){
+        void Move(short int MoveN){
             switch (MoveN)
             {
-            case 0:         // Μένει ακίνητο
-                break;
             case 1:         // Κινείται πάνω
                 this->y--;
                 break;
-            case 2:         // Κινείται κάτω
-                this->y++;
-                break;
-            case 3:         // Κινείται αριστερα
+            case 3:         // Κινείται αριστερά
                 this->x--;
                 break;
             case 4:          // Κινείται δεξιά
                 this->x++;
-                break;    
+                break;
+            case 6:         // Κινείται κάτω
+                this->y++;
+                break;
             }
         }
 };
@@ -284,22 +281,22 @@ class Map{
             Grid[rand() % n][rand() % m] = 'P';
         }
 
-        void SetCoordinates(Entity* Obj)
+        void SetCoordinates(Entity* Obj) //εδώ γίνεται το λάθος 
         {
-            int x = rand() % n;
-            int y = rand() % m;
+            int y = rand() % n;
+            int x = rand() % m;
             while(1)
             {
-                if (this->Grid[x][y] == '-')
+                if (this->Grid[y][x] == '-')
                 {
                     // Έλεγχος για τον αποκλεισμό των NPC και του Avatar από terrain (γη, νερό, γιατρικό, όρια του map), κατά την αρχή του παιχνιδιού
                     char* array = new char;
-                    array = Scan(x, y);
+                    array = Scan(y, x);
 
-                    int i = 0;
+                   /* int i = 0;
                     if (Obj->getID() == 'w' || Obj->getID() == 'W' || Obj->getID() == 'V')
                     {
-                        for (i = 1; i < 8; i+=2){
+                        for (i = 0; i < 7; i+=2){
                             if (array[i] != 'E' && array[i] != 'S' && array[i] != 'T' && array[i] != 'P') break;
                         }
                         
@@ -315,17 +312,18 @@ class Map{
                         
                         delete[] array;
                     }
-                    if (i == 9 && (Obj->getID() == 'w' || Obj->getID() == 'W' || Obj->getID() == 'V') || i == 8 && Obj->getID() == 'v') break;
+                    if (i == 9 && (Obj->getID() == 'w' || Obj->getID() == 'W' || Obj->getID() == 'V') || i == 8 && Obj->getID() == 'v') break;*/
 
                     // Αρχικοποίηση των συντεταγμένων του NPC ή του Avatar
-                    Obj->SetXY(x, y);
-                    Grid[x][y]=Obj->getID();
+                    Obj->SetYX(y, x);
+                    Grid[y][x]=Obj->getID();
+                    delete [] array;
                     return;
                 }
 
                 // Εύρεση καινούργιων συντεταγμένων για το NPC ή το Avatar
-                x = rand() % n;
-                y = rand() % m;
+                y = rand() % n;
+                x = rand() % m;
             }
         }
 
@@ -335,24 +333,26 @@ class Map{
             {
                 objvecV.push_back(new Vampire);
                 SetCoordinates(objvecV.at(i));
+                cout<<objvecV.at(i)->getID()<<endl;
             }
             for (int i = 0; i < NumOfNPC; i++)
             {   
                 objvecW.push_back(new Werewolf);
                 SetCoordinates(objvecW.at(i));
+                cout<<objvecW.at(i)->getID()<<endl;
             }
         }
 
         //H scan επιστρεφει array bool για τις θέσεις γύρω από μία θέση για κάθε αντικείμενο
 
-        char* Scan(int x, int y)
+        char* Scan(int y, int x)
         {
             // Το NearPositions[] περιλαμβάνει το max 3*3 πλαίσιο του Grid, εκτός της θέσης με την οποία καλείται, με την σειρά: από πάνω αριστερά έως κάτω δεξιά
             char* NearPositions=new char[8];
             int NP_index=0;
-            for (int j = y - 1; j <= y + 1; j++)
+            for (int i = y - 1; i <= y + 1; i++)
             {
-                for (int i = x - 1; i <= x + 1; i++)
+                for (int j = x - 1; j <= x + 1; j++)
                 {
                     if (i < 0 || i > n - 1 || j < 0 || j > m - 1)
                     {
@@ -361,23 +361,25 @@ class Map{
                         continue;
                     }
 
-                    if (i == x && j == y)
+                    if (i == y && j == x)
                         continue;
 
                     NearPositions[NP_index] = Grid[i][j];
                     NP_index++;
                 }
             }
-            return NearPositions; // NearPositions[περιττού αριθμού] = πάνω, κάτω, δεξιά, αριστερά
+            return NearPositions;
         }
 
         int Iterate(vector<NPC*> objvec, NPC* Target)
         {
-            for (int i; i < objvec.size(); i++)
+            int i;
+            for (i = 0; i < objvec.size(); i++)
             {
-                if (Target == objvec.at(i))
-                   return i;
+                if (Target == objvec.at(i)) break;
             }
+            cout << i << endl;
+            return i;
         }
 
         NPC* GetNPC(vector<NPC*> objvec,int &x,int &y,short int block){
@@ -420,92 +422,117 @@ class Map{
                     break;
             }
             
-            for (int i=0;i<objvec.size();i++) {
-                if (objvec.at(i)->getX() == x && objvec.at(i)->getX() == y) return objvec.at(i);
+            int i;
+            for (i=0;i<objvec.size();i++) { //to tsekara kai den exei provlhma, kanonika to gyrnaei. sthn heal kai sthn attack yparxei provlhma
+                if (objvec.at(i)->getX() == x && objvec.at(i)->getY() == y) {
+                     break;
+                }    
             }
+            return objvec.at(i);
         }
         
         void Action(NPC* protagonist)
         {
             char* array = new char;
-            array = Scan(protagonist->getX(), protagonist->getY());
+            array = Scan(protagonist->getY(), protagonist->getX());
+            for (int i=0;i<8;i++){
+                cout << array[i] << endl;
+            }
 
             short int block = rand() % 8;
+            cout << "block :" << array[block] << endl;
+            cout << "id " << protagonist->getID() << endl;
+            
 
+            // Η θέση που επιλέγεται δεν μπορεί προσπελαστεί από το NPC, οπότε, αυτό θα μείνει ακίνητο
             if (array[block] == 'E' || array[block] == 'S' || array[block] == 'T' || array[block] == 'P' || array[block] == 'W' || array[block] == 'V') {
+                cout << "MENEI STASIMO" << endl;
+                delete[] array; //EDW EXEI BUG! Diagrafetai to id tou protagonist alla oxi sto idio, sto vector ! 
                 return;
             }
             
             int x1 = protagonist->getX();
             int y1 = protagonist->getY();
-            NPC* Target= new NPC;
+            NPC* Target = new NPC;
 
+            // Η θέση που επιλέγεται περιέχει σύμμαχο (NPC της ίδιας ομάδας)
             if (array[block] == protagonist->getID()) {
-                if (protagonist->getID() == 'w') Target = GetNPC(objvecW, x1, y1, block);
-                else Target = GetNPC(objvecV, x1, y1, block);
-                
+                if (protagonist->getID() == 'w'){
+                    Target = GetNPC(objvecW, x1, y1, block);
+                } 
+                else {
+                    Target = GetNPC(objvecV, x1, y1, block);
+                }
+                // Αν το NPC έχει γιατρικό και το επίπεδο υγείας του συμμάχου δεν είναι πλήρες, τότε το NPC θα αναπληρώσει ένα επίπεδο υγείας του συμμάχου του, με κόστος ενος γιατρικού του
                 if (protagonist->getPotions() > 0 && Target->getCurrentHP() < Target->getCapHP()){
                     protagonist->Heal(Target);
-                    
-                    delete Target;
-                    delete[] array;
-                    return;
                 }
+                cout << Target->getID()<<endl;
+
+                // Αν δεν ικανοποιούνται οι απαραίτητες συνθήκες για την Heal(), τότε το NPC μένει ακίνητο
+                delete[] array;
+                //delete Target;
+                return;
             }
 
+            // Η θέση που επιλέγεται περιέχει εχθρό
             if (array[block] != protagonist->getID() && array[block] != '-') {
-                if (protagonist->getID() == 'w') Target = GetNPC(objvecV, x1, y1, block);
-                else Target = GetNPC(objvecW, x1, y1, block);
-                
+                if (protagonist->getID() == 'w') {
+                    Target = GetNPC(objvecV, x1, y1, block);
+                }    
+                else {
+                    Target = GetNPC(objvecW, x1, y1, block);
+                }
+                // Αν το επίπεδο δύναμης του εχθρού είναι μικρότερο, τότε επιτίθεται
                 if (protagonist->getPower_Lvl() >= Target->getPower_Lvl())
                 {
                     protagonist->Attack(Target);
+                    
+                    // Αν ο εχθρός πεθάνει, διαγράφεται από το αντίστοιχο vector
                     if (Target->getCurrentHP() <= 0)
                     {
                         int TargetIndex;
             
                         if (Target->getID() == 'w') {
                             TargetIndex = Iterate(objvecW,Target);
-                            delete objvecW.at(TargetIndex);
+                            Grid[y1][x1] = '-';
                             objvecW.erase(objvecW.begin()+TargetIndex);
-                        }    
+                        }
                         else {
                             TargetIndex = Iterate(objvecV,Target);
-                            delete objvecV.at(TargetIndex);
-                            objvecW.erase(objvecV.begin()+TargetIndex);
+                            Grid[y1][x1] = '-';
+                            objvecV.erase(objvecV.begin()+TargetIndex);
                         }
                     }
-                    
-                    delete Target;
-                    delete[] array;
-                    return;
                 }
-                /*else
-                {
-                    protagonist->Move();
-                    return;
-                }*/
+                      
+                delete[] array;
+                //delete Target;
+                return;       
             }
 
-            //if (array[block] == '-') protagonist->Move();
-            
-            /*if (block % 2 != 0 && protagonist->getID() == "w" ) {
-                
+            // Στην περίπτωση που ένα werewolf NPC πάρει διαγώνια, άδεια θέση, μένει ακίνητο
+            if (block % 2 == 0 && protagonist->getID() == 'w') {
+                cout << "PITHANH MALAKIA " << endl;
+                delete[] array;
+                //delete Target;
+                return;
             }
-            
-                    // Attack cases
-                    if (
 
-                    //heal case
-            */
-            
-            delete Target;
+            // Αν η θέση που επιλεχθεί είναι άδεια, τότε το NPC κινείται σε αυτή
+            else if (array[block] == '-') {  //Για κάποιον λόγο σε αυτό το σημείο ενώ το ελέγχει δεν ξέρει αν υπάρχει ή όχι κάποιο αντικείμενο
+                Grid[protagonist->getY()][protagonist->getX()] = '-';
+                protagonist->Move(block);
+                Grid[protagonist->getY()][protagonist->getX()] = protagonist->getID();
+            }
+
+            //delete Target;
             delete[] array;
         }
 
         void print(){
-            for (int j=0;j<m;j++){
-                for (int i=0;i<n;i++){
+            for (int i=0;i<n;i++){
+                for (int j=0;j<m;j++){
                     cout << this->Grid[i][j];
                 }
                 cout << endl;
@@ -524,7 +551,7 @@ class Game{
     public:
         void Input(bool *k,Map newMap,Avatar Player)
         {
-            Player.Move(newMap.Scan(Player.getX(), Player.getY()), k);
+            Player.Move(newMap.Scan(Player.getY(), Player.getX()), k);
         }
 };
 
@@ -532,35 +559,8 @@ class Game{
 
 int main() {
     srand(time(0));
-    n=20;
-    m=20;
-
-    //Werewolf lykaki;
-    //Vampire drakoulas;
-      /* for (int i=0;i<5;i++){
-        cout << "Vampire original location" << endl;
-        int VampX=drakoulas.getX();
-        cout <<"X:" << VampX << endl;
-        int VampY=drakoulas.getY();
-        cout <<"Y:" << VampY << endl;
-        drakoulas.Move(VampX, VampY);
-        cout << "Vampire final location" << endl;
-        VampX=drakoulas.getX();
-        cout <<"X:" << VampX << endl;
-        VampY=drakoulas.getY();
-        cout <<"Y:" << VampY << endl;
-        cout << "Werewolf original location" << endl;
-        int WereX=lykaki.getX();
-        cout <<"X:" << WereX << endl;
-        int WereY=lykaki.getY();
-        cout <<"Y:" << WereY << endl;
-        lykaki.Move(WereX,WereY);
-        cout << "Werewolf final location" << endl;
-        WereX=lykaki.getX();
-        cout <<"X:" << WereX << endl;
-        WereY=lykaki.getY();
-        cout << "Y:" << WereY << endl;
-    } */
+    n=10;
+    m=12;
 
     Map newMap;
     Game newGame;
@@ -568,9 +568,68 @@ int main() {
     newMap.SetCoordinates(&Player);
 
     newMap.SpawnEntities();
+    int num = 0;
+    while (1){
+
+        cout << endl;
+        for (int i=0; i<newMap.objvecW.size(); i++) {
+            newMap.print();
+            cout << i << endl;
+            // if (newMap.objvecW.at(i)->getID() != 'w'){
+            //     cout << "TI SKATA EGINE!" << endl;
+            //     cout << newMap.objvecW.at(i)->getID() << endl;
+                cout << "CURRENT HP:" << newMap.objvecW.at(i)->getCurrentHP() << endl;
+                cout << "Potions: " << newMap.objvecW.at(i)->getPotions() << endl;
+            //     cout << newMap.objvecW.size() << endl;
+            //     cout << i << endl;
+            //     return 0;
+            // }
+            cout << newMap.objvecW.at(i)->getID() << " "<<newMap.objvecW.at(i)->getY() << " " <<newMap.objvecW.at(i)->getX()  << endl;
+            newMap.Action(newMap.objvecW.at(i));
+            cout << "Kinish:" << newMap.objvecW.at(i)->getID() << " "<<newMap.objvecW.at(i)->getY() << " " <<newMap.objvecW.at(i)->getX()  << endl;
+
+            for (int i = 0; i<newMap.objvecW.size();i++){
+                cout << newMap.objvecW.at(i)->getID() <<endl;
+            }
+            cout << endl;
+
+        }   
+        for (int i=0; i<newMap.objvecV.size(); i++) {
+            newMap.print();
+            cout << i << endl;
+            // if (newMap.objvecV.at(i)->getID() != 'v'){
+            //     cout << "TI SKATA EGINE!" << endl;
+            //     cout << newMap.objvecV.at(i)->getID() << endl;
+                cout << "CURRENT HP:" << newMap.objvecV.at(i)->getCurrentHP() << endl;
+                cout << "Potions: " << newMap.objvecV.at(i)->getPotions() << endl;
+            //     cout << newMap.objvecV.size() << endl;
+            //     cout << i << endl;
+            //     return 0;
+            // }
+            cout << newMap.objvecV.at(i)->getID() << " "<<newMap.objvecV.at(i)->getY() << " " <<newMap.objvecV.at(i)->getX()  << endl;
+            newMap.Action(newMap.objvecV.at(i));
+            cout << "Kinish:" << newMap.objvecV.at(i)->getID() << " "<<newMap.objvecV.at(i)->getY() << " " <<newMap.objvecV.at(i)->getX()  << endl;
+
+            for (int i = 0; i<newMap.objvecV.size();i++){
+                cout << newMap.objvecV.at(i)->getID() <<endl;
+            }
+            cout << endl;
+
+        }   
+        
+        //Sleep(2000);
+        newMap.print();
+        cout << endl;
+        if (newMap.objvecV.size() == 0 || newMap.objvecW.size() == 0 ){
+            cout << "Paixnidi teleiwse" << endl;
+            break;
+        }
+
+       
+    }
+    
     newMap.print();
+    
     return 0;
 }
-
-
 // Γύρος: κάθε αντικείμενο εκτελεί μόνο μία ενέργεια. Δυνατές ενέργειες: μένει ακίνητο, αλλάζει θέση, επουλώνει πληγές συμμάχου, επιτίθεται σε αντίπαλο
